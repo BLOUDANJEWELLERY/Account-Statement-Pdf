f600529a41fc78c00ce1c004de734d5f4d0d6ea1
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2026-01-01 23:52:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = 'offline-cache-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html'
+const CACHE_NAME = 'offline-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
